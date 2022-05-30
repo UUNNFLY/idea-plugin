ab3c615da5b9457dbfa038d497119afc26da3c5c
GitHub Action <action@github.com>
2022-05-30 15:37:49 +0000

    Template cleanup

--- src/main/kotlin/com/github/uunnfly/ideaplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/uunnfly/ideaplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.uunnfly.ideaplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.uunnfly.ideaplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
